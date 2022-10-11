fddbf2945e6e332f1334fb7a6076e2fb0c75077e
olyatupchiy <olya.tupchiy@gmail.com>
2022-10-11 12:18:55 +0200

    Added html file with basic structure and js file with arithmetic mean

---
+++ index.js
@@ -0,0 +1,5 @@
+const number1 = prompt("Enter the first number");
+const number2 = prompt("Enter the second number");
+const number3 = prompt("Enter the third number");
+const result = (+number1 + +number2 + +number3) / 3;
+alert(result);
